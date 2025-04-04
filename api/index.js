af4e85d4320e32947026638f4e1dddb7d82423c2
Fabio Nitto <fabio.nitto@gmail.com>
2025-04-04 14:05:09 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,17 +9,11 @@ export default function (req) {
   // Get the original IP address using ipAddress(req)
   const originalIp = ipAddress(req);
 
-  // Extract the X-Forwarded-For header
-  const xForwardedFor = req.headers.get("X-Forwarded-For");
-
-  // If the header exists, split it into an array of IP addresses
-  const forwardedIps = xForwardedFor ? xForwardedFor.split(",").map(ip => ip.trim()) : ["IP not found"];
-
   // Get CF-Connecting-IP from Cloudflare
   const clientIp = req.headers.get("CF-Connecting-IP");
 
   // Concatenate the original IP and the X-Forwarded-For IPs
-  const combinedIps = [originalIp, clientIp, ...forwardedIps].join(", ");
+  const combinedIps = ["IP Origem",originalIp,"\n","Ip Cliente CF", clientIp].join(", ");
 
   return new Response(combinedIps);
 }
